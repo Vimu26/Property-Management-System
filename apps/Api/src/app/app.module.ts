d8867852eacf7149d3a93b7bd9e506ea138dc089
Vimu26 <akalankavimukthi2@gmail.com>
2024-12-15 16:44:34 +0530

    pipeline issues fixed

--- apps/Api/src/app/app.module.ts
+++ apps/Api/src/app/app.module.ts
@@ -1,10 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
-
 
 @Module({
   imports: [
@@ -18,9 +17,12 @@ import { HttpModule } from '@nestjs/axios';
       useFactory: (config: ConfigService) => {
         const uri = config.get<string>('DB_URL');
         if (!uri) {
-          throw new Error('DB_URL is not defined in the environment variables.');
+          throw new Error(
+            'DB_URL is not defined in the environment variables.'
+          );
         }
-        return { uri }
+        Logger.log(`🚀 DB Connected in url ${uri}`);
+        return { uri };
       },
     }),
     HttpModule,
