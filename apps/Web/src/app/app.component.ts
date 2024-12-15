8cf877bf8bcc9dfb6da4a00d1d2693b48311c27d
Vimu26 <akalankavimukthi2@gmail.com>
2024-12-15 12:22:53 +0530

    fix: standalone component issues fixed

--- apps/Web/src/app/app.component.ts
+++ apps/Web/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule ,CommonModule],
   selector: 'app-root',
+  standalone : true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
