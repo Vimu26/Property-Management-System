f72223899b498e2792d4e5a2059947c752a089d7
Vimu26 <akalankavimukthi2@gmail.com>
2024-12-15 15:50:01 +0530

    feat: nest js dependencies added and mongo db connected

--- apps/Api/src/app/app.module.ts
+++ apps/Api/src/app/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const uri = config.get<string>('DB_URL');
+        if (!uri) {
+          throw new Error('DB_URL is not defined in the environment variables.');
+        }
+        return { uri }
+      },
+    }),
+    HttpModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
