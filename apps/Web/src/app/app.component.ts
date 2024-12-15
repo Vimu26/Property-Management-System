d50bb7429f8165d657469d3003a59a7878578a62
Vimu26 <akalankavimukthi2@gmail.com>
2024-12-15 15:05:42 +0530

    material module added

--- apps/Web/src/app/app.component.ts
+++ apps/Web/src/app/app.component.ts
@@ -2,21 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { CommonModule } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatButtonModule } from '@angular/material/button';
+import { MaterialModule } from './material.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    NxWelcomeComponent,
-    RouterModule,
-    CommonModule,
-    MatButtonModule,
-    MatDividerModule,
-    MatIconModule,
-  ],
+  imports: [NxWelcomeComponent, RouterModule, CommonModule, MaterialModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
