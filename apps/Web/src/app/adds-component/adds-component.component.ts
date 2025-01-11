7cd638992e2eb0b5337f9e32c04f29030579826b
Vimu26 <akalankavimukthi2@gmail.com>
2025-01-11 20:59:55 +0530

    adds component added

---
+++ apps/Web/src/app/adds-component/adds-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-adds-component',
+  imports: [CommonModule],
+  standalone: true,
+  templateUrl: './adds-component.component.html',
+  styleUrl: './adds-component.component.scss',
+})
+export class AddsComponentComponent {}
--- apps/Web/src/app/app.component.ts
+++ apps/Web/src/app/app.component.ts
@@ -4,11 +4,18 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material.module';
 import { HeaderComponentComponent } from './header-component/header-component.component';
+import { AddsComponentComponent } from './adds-component/adds-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, CommonModule, MaterialModule , HeaderComponentComponent],
+  imports: [
+    RouterModule,
+    CommonModule,
+    MaterialModule,
+    HeaderComponentComponent,
+    AddsComponentComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
