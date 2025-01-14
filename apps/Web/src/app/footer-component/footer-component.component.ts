102829b3250d202c356e1d7223104eec1cf25c27
Vimu26 <akalankavimukthi2@gmail.com>
2025-01-14 20:42:38 +0530

    footer component created

---
+++ apps/Web/src/app/footer-component/footer-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-footer-component',
+  imports: [CommonModule],
+  templateUrl: './footer-component.component.html',
+  styleUrl: './footer-component.component.scss',
+})
+export class FooterComponentComponent {}
